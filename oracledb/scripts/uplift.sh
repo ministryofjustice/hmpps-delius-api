039d5ff22766937af6bd6eac4919b71f95e5c141
marcus.aspin <maspin@unilink.com>
2021-03-18 17:48:08 +0000

    Enabled recreation of schemas + disable global security during uplift

--- oracledb/scripts/uplift.sh
+++ oracledb/scripts/uplift.sh
@@ -17,7 +17,12 @@ sed -i 's/conn SYS\/\&\&P_SYS_PASSWORD /conn SYS\/\&\&P_SYS_PASSWORD@XEPDB1 /' .
 sed -i 's/conn \&\&P_TARGET_SCHEMA\/\&\&P_APP_SCHEMA_PASSWORD/conn \&\&P_TARGET_SCHEMA\/\&\&P_APP_SCHEMA_PASSWORD@XEPDB1 /' ../Operational\ Database\ Scripts/001_create_application_schema.sql
 sed -i '1s/^/ALTER SESSION SET CONTAINER=XEPDB1;\n/' ../Operational\ Database\ Scripts/002_grants.sql
 
-# Run the 008 script twice, it fails the first time
+# Update to run the 001 script for recreating schemas, and disable global security (not supported in XE)
+sed -i 's/RUN_001_SCRIPT="N"/RUN_001_SCRIPT="Y"/' install_operational_scripts.sh
+sed -i "s/p_global => 'Y'/p_global => 'N'/" ../Operational\ Database\ Scripts/001_create_application_schema.sql
+sed -i "s/p_password => 'GLOBALLY'/p_password => l_delius_p/" ../Operational\ Database\ Scripts/001_create_application_schema.sql
+
+# Update to run the 008 script twice, it fails the first time
 sed -i 's/.*008_SPG_Setup.sql.*/&\n&/' install_operational_scripts.sh
 
 # Run uplift scripts
