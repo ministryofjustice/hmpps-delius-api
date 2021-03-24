c4d311eaa8254d53dba3ab8a18f213aac8a2ebc0
Ed Brindley <ed@maidavale.org>
2021-03-24 11:39:58 +0000

    DAPI-53 Implement transactions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/advice/AuditableAspect.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/advice/AuditableAspect.kt
@@ -5,6 +5,8 @@ import org.aspectj.lang.annotation.AfterThrowing
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.slf4j.LoggerFactory
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.deliusapi.exception.BadRequestException
 import uk.gov.justice.digital.hmpps.deliusapi.service.audit.AuditContext
@@ -17,6 +19,7 @@ annotation class Auditable(val interaction: AuditableInteraction)
 
 @Aspect
 @Component
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
 class AuditableAspect(private val auditService: AuditService) {
   companion object {
     private const val JOIN_POINT = "@annotation(auditable)"
