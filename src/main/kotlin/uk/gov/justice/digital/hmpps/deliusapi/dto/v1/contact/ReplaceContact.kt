19bbcb77f21e3dae537693aee507362ff1288fef
marcus.aspin <maspin@unilink.com>
2021-05-18 13:12:12 +0100

    DAPI-168 Added tests for DTO/Bean validation, and fixed some validation rules

--- src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/dto/v1/contact/ReplaceContact.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/deliusapi/dto/v1/contact/ReplaceContact.kt
@@ -4,18 +4,22 @@ import uk.gov.justice.digital.hmpps.deliusapi.validation.Crn
 import uk.gov.justice.digital.hmpps.deliusapi.validation.EndTime
 import uk.gov.justice.digital.hmpps.deliusapi.validation.FieldGroup
 import uk.gov.justice.digital.hmpps.deliusapi.validation.FieldGroupType
+import uk.gov.justice.digital.hmpps.deliusapi.validation.FieldGroups
+import uk.gov.justice.digital.hmpps.deliusapi.validation.NotBlankWhenProvided
 import uk.gov.justice.digital.hmpps.deliusapi.validation.StartTime
+import uk.gov.justice.digital.hmpps.deliusapi.validation.TimeRanges
 import java.time.LocalDate
 import java.time.LocalTime
-import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Positive
 import javax.validation.constraints.Size
 
+@TimeRanges
+@FieldGroups
 data class ReplaceContact(
   @Crn
   val offenderCrn: String,
 
-  @NotBlank
+  @NotBlankWhenProvided
   @field:Size(max = 10)
   val outcome: String,
 
